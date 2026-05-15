e3229822d128ce7e4be76c507c56af6d26b9193f
Rylan Deacon-Rogers <rylan.deaconrogers@hotmail.com>
2026-05-14 19:25:21 -0700

    Bug fix

--- sflhubdevelopment/src/app/components/btnWhite.js
+++ sflhubdevelopment/src/app/components/btnWhite.js
@@ -1,9 +1,16 @@
-export default function BtnWhite({ text, onClick, Icon }) {
+export default function BtnWhite({
+  text,
+  onClick,
+  Icon,
+  type = "button",
+  ...rest
+}) {
   return (
     <button
-      type="button"
+      type={type}
       onClick={onClick}
       className="bg-white py-2 px-3 hover:bg-green-950 hover:border hover:text-white text-green-950 font-bold flex my-auto"
+      {...rest}
     >
       <Icon className="mr-2 my-auto" />
       {text}
